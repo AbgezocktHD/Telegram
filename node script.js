337b622164972aca34f2bc2ed42475936127d580
AbgezocktHD <34423179+AbgezocktHD@users.noreply.github.com>
2017-12-10 19:43:51 +0100

    Create node script.js

---
+++ node script.js
@@ -0,0 +1,18 @@
+(code lang: javascript)
+// index.js
+const token = '478346808:AAHPSiuiEV19ce1zvovIGcLJH5IgD8qj4yY';
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/hello/, function (msg) {
+bot.sendMessage(msg.chat.id, Hello ${msg.from.first_name}!);
+});
+
+console.log('bot started');
+(/code)
+
+Gestartet wird das Script mittels Node.js:
+
+(code lang: bash)
+node index.js
+(/code)
